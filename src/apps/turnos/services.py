6be035b7f3afe31fb2a6ac61e10489c6b0e4c544
CarlosMtz27 <carlosmartinezhernandez1930@gmail.com>
2026-01-22 12:11:56 -0600

    Se agrego el metodo para cerrar turno en services

--- src/apps/turnos/services.py
+++ src/apps/turnos/services.py
@@ -1,9 +1,10 @@
 from django.core.exceptions import ValidationError
+from django.db import transaction
+from django.db.models import Sum
+
 from .models import Turno
 from apps.caja.models import MovimientoCaja
-from django.db.models import Sum
-from django.utils import timezone
-from django.db import transaction
+
 
 @transaction.atomic
 def iniciar_turno(*, usuario, tipo_turno, caja_inicial=0, sueldo=0):
@@ -11,11 +12,9 @@ def iniciar_turno(*, usuario, tipo_turno, caja_inicial=0, sueldo=0):
     Inicia un turno asegurando que solo exista uno activo.
     """
 
-    # 1️⃣ Verificar si ya existe un turno activo
     if Turno.objects.filter(activo=True).exists():
         raise ValidationError("Ya existe un turno activo")
 
-    # 2️⃣ Crear el turno
     turno = Turno.objects.create(
         usuario=usuario,
         tipo_turno=tipo_turno,
@@ -26,36 +25,43 @@ def iniciar_turno(*, usuario, tipo_turno, caja_inicial=0, sueldo=0):
 
     return turno
 
-def cerrar_turno(*, usuario, sueldo_reportado, efectivo_reportado):
-    try:
-        turno = Turno.objects.get(usuario=usuario, activo=True)
-    except Turno.DoesNotExist:
-        raise ValidationError("No tienes un turno activo")
 
-    # Sumar solo efectivo
+@transaction.atomic
+def cerrar_turno_service(
+    *,
+    turno: Turno,
+    efectivo_reportado,
+    sueldo
+):
+    """
+    Cierra un turno calculando el efectivo esperado.
+    """
+
+    if not turno.activo:
+        raise ValidationError("El turno ya está cerrado")
+
     total_efectivo = (
         MovimientoCaja.objects
         .filter(
             turno=turno,
             metodo_pago=MovimientoCaja.MetodoPago.EFECTIVO
         )
-        .aggregate(total=Sum("monto"))["total"] or 0
+        .aggregate(total=Sum("monto"))["total"]
+        or 0
     )
 
+    #Calcular efectivo esperado
     efectivo_esperado = (
         turno.caja_inicial
         + total_efectivo
-        - sueldo_reportado
+        - sueldo
     )
 
-    diferencia = efectivo_reportado - efectivo_esperado
-
-    turno.sueldo_reportado = sueldo_reportado
-    turno.efectivo_reportado = efectivo_reportado
-    turno.efectivo_esperado = efectivo_esperado
-    turno.diferencia = diferencia
-    turno.activo = False
-    turno.fecha_fin = timezone.now()
-    turno.save()
+    #Cerrar turno usando el modelo
+    turno.cerrar_turno(
+        efectivo_esperado=efectivo_esperado,
+        efectivo_reportado=efectivo_reportado,
+        sueldo=sueldo
+    )
 
     return turno
